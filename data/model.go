60a1c149809d1e65d0101d89ad3e69cb208cf572
iqballazuardi <iqballazuardi12@gmail.com>
2023-03-22 00:19:51 +0700

    menambahkan fitur (tambah pegawai,hapus pegawai, daftar pegawai)

--- data/model.go
+++ data/model.go
@@ -2,6 +2,8 @@ package data
 
 import (
 	"database/sql"
+	"errors"
+	"fmt"
 )
 
 type Model struct {
@@ -10,4 +12,72 @@ type Model struct {
 
 func (m *Model) SetSQLConnection(db *sql.DB) {
 	m.conn = db
+}
+
+func (m *Model) TambahPegawai(newPegawai Pegawai) error {
+	res, err := m.conn.Exec("INSERT INTO pegawai (nama, username, password,email) values(?,?,?,?)", newPegawai.Nama, newPegawai.Username, newPegawai.Password, newPegawai.Email)
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	aff, err := res.RowsAffected()
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if aff <= 0 {
+		return errors.New("terjadi sebuah masalah pada sistem")
+	}
+
+	m.conn.Close()
+
+	return nil
+}
+func (m *Model) DeletePegawai(Email string) error {
+	res, err := m.conn.Exec("DELETE FROM pegawai WHERE email = ?", Email)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	aff, err := res.RowsAffected()
+
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	if aff <= 0 {
+		return errors.New("terjadi sebuah masalah pada sistem")
+	}
+
+	m.conn.Close()
+
+	return nil
+}
+
+func (m Model) GetAllPegawai() ([]Pegawai, error) {
+	listPegawai := []Pegawai{}
+	rows, err := m.conn.Query("SELECT idpegawai, nama, username FROM pegawai")
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	for rows.Next() {
+		var newPegawai = Pegawai{}
+		if err := rows.Scan(&newPegawai.Id, &newPegawai.Nama, &newPegawai.Username); err != nil {
+			fmt.Println(err)
+			return nil, err
+		}
+		listPegawai = append(listPegawai, newPegawai)
+	}
+	m.conn.Close()
+
+	return listPegawai, nil
 }
