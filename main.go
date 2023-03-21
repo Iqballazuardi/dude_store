7281ab60f26f88be50aef05d25533e7b215a31b1
iqballazuardi <iqballazuardi12@gmail.com>
2023-03-21 20:23:18 +0700

    Melengakapi skeleton code

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"dudeStore/dudeStore/config"
 	"dudeStore/dudeStore/data"
+	"fmt"
 )
 
 func main() {
@@ -10,4 +11,50 @@ func main() {
 	mdl := data.Model{}
 	mdl.SetSQLConnection(koneksi)
 
+	var username string
+	var password string
+
+	fmt.Println("Silahkan login")
+	fmt.Println("Masukan username")
+	fmt.Scanln(&username)
+	fmt.Println("Masukan password")
+	fmt.Scanln(&password)
+
+	if username == "admin" {
+		var menu int
+		for menu != 99 {
+			fmt.Println("Selamat datang admin")
+			fmt.Println("1.Tambahkan Pegawai")
+			fmt.Println("2.Hapus Pegawai")
+			fmt.Println("3.Daftar Pegawai")
+			fmt.Println("4.Tambahkan Produk")
+			fmt.Println("5.update Produk")
+			fmt.Println("6.Hapus Produk")
+			fmt.Println("7.Tambahkan Pelanggan")
+			fmt.Println("8.Hapus Pelanggan")
+			fmt.Println("9.Tambahkan Transaksi")
+			fmt.Println("10.Daftar Transaksi")
+			fmt.Println("11.Hapus Transaksi")
+			fmt.Println("0.Log Out")
+			fmt.Scanln(&menu)
+			if menu == 0 {
+				break
+			}
+		}
+	} else {
+		var menu int
+		for menu != 99 {
+			fmt.Println("Selamat datang pegawai")
+			fmt.Println("1.Tambahkan Pelanggan")
+			fmt.Println("2.Tambahkan Produk")
+			fmt.Println("3.Update Produk")
+			fmt.Println("4.Daftar Produk")
+			fmt.Println("5.Daftar Pelanggan")
+			fmt.Println("0.Log Out")
+			fmt.Scanln(&menu)
+			if menu == 0 {
+				break
+			}
+		}
+	}
 }
